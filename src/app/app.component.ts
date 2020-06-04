d6883e39756bda7f4edf044465194320827e02fd
m0nq <forgetmenaught@gmail.com>
2020-06-04 11:30:33 -0700

    Add bluma styles and faker packaging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
-  selector: 'cq-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'cq-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+    randomText = lorem.sentence();
 }
